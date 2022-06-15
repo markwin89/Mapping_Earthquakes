af0bb2ea09f5a704e33b5594b11dca8c6a1aa1fa
Mark Nguyen <101272613+markwin89@users.noreply.github.com>
2022-06-15 12:19:22 -0400

    Add files via upload

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,12 +4,12 @@
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//  Add a marker to the map for Los Angeles, California.
-// L.circleMarker([34.0522, -118.2437], {
-//   radius: 300,
-//   color: "black",
-//   fillColor: '#ffffa1'
-// }).addTo(map);
+// Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
 
  //  Add a marker to the map for Los Angeles, California.
  // let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -26,22 +26,3 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-// cities.forEach(function(city) {
-//   console.log(city)
-//   L.marker(city.location).addTo(map);
-//  });
-
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-.addTo(map);
-});
